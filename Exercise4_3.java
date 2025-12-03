170a87502c78faeea3fe3624f42948bca109cd3d
255541002AsimGokalp <255541002@firat.edu.tr>
2025-12-03 22:02:24 +0300

    Update Exercise4_3.java
    3. egzersiz tamamlandı

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -5,7 +5,20 @@ public class Exercise4_3 {
         System.out.print("You wugga ");
         baffle();
     }
+	/** 1. sory: Bu soruda, ping metodunun ilk kez çağrıldığı anda çağrı yığınının (stack) durumunu olduu. Program  main metodundan başlıyor. Önce main içindeki System.out.print("No, I "); çalışıyor, ardından zoop(); çağrılıyor. Bu anda çağrı yığınının en altında main, onun üstünde de zoop metodu bulunuyor.
+zoop metodunun içinde bu kez baffle(); çağrılıyor. Böylece stack’e bir seviye daha ekleniyor ve baffle en üstte olacak şekilde sıralama main → zoop → baffle haline geliyor. baffle metodu System.out.print("wug"); satırını çalıştırdıktan sonra ping(); metodunu çağırıyor.
+İşte ping metodunun ilk kez çağrıldığı anda çağrı yığını şu şekilde oluyor:
+En altta: main(String[] args)
+Onun üzerinde: zoop()
+Onun üzerinde: baffle()
+En üstte (aktif olan): ping()
+Yani alttan üste doğru çağrı sırası: main → zoop → baffle → ping şeklindedir.
+2. soru
+No, I wug.
+You wugga wug.
+I wug.
 
+    */
     public static void main(String[] args) {
         System.out.print("No, I ");
         zoop();
