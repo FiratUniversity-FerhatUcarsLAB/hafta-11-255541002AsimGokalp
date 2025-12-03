3117e9f4e5ed2d9ab22d18afd8c798603bb18854
255541002AsimGokalp <255541002@firat.edu.tr>
2025-12-03 21:47:10 +0300

    Update Exercise4_1.java
    Assignment1 was completed

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -2,13 +2,26 @@ public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
         // TODO
+        System.out.println("American Format");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
         // TODO
+        System.out.println("European Format");
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
+    
 
     public static void main(String[] args) {
         // TODO: test methods
+                String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        printAmerican(day, date, month, year);
+        System.out.println(); // bos satir
+        printEuropean(day, date, month, year);
     }
 }
